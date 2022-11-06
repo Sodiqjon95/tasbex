d9124938aa50c27fe011283621f4e4a6943d6097
Sodiqjon95 <sodiqdev95@gmil.com>
2022-11-06 15:07:40 +0500

    Initial commit

--- lib/data_screen.dart
+++ lib/data_screen.dart
@@ -1,38 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tasbex/storage_service.dart';
 
+// ignore: must_be_immutable
 class DataScreen extends StatefulWidget {
-  const DataScreen({Key? key, required this.myListener}) : super(key: key);
-
-  final ValueChanged<String> myListener;
+  DataScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<DataScreen> createState() => _DataScreenState();
 }
 
 class _DataScreenState extends State<DataScreen> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark, statusBarColor: Colors.transparent));
     return Scaffold(
-      body: Container(
-        // color: Colors.red,
-        width: double.infinity,
-        height: double.infinity,
-        child: Stack(
-          children: [
-            Align(child: Image.asset("assets/images/tasbex_png.png")),
-            const Positioned(
-              left: 0,
-              right: 0,
-              // bottom: 0,
-              top: 285,
-              child: Text(
-                "0",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 60),
-              ),
-            )
-          ],
-        ),
+      body: Stack(
+        children: [
+          Align(
+              child: Image.asset(
+            "assets/images/tasbex_png-removebg-preview.png",
+            width: 400.w,
+          )),
+          Positioned(
+            left: 90.w,
+            right: 90.w,
+            top: 260.h,
+            child: Text(
+              count.toString(),
+              textAlign: TextAlign.end,
+              style: TextStyle(fontSize: 60.sp),
+            ),
+          ),
+          Positioned(
+            left: 0.w,
+            right: 0.w,
+            top: 365.h,
+            child: Container(
+                width: 130.w,
+                height: 130.h,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100.r)),
+                child: FloatingActionButton(
+                  backgroundColor: Colors.transparent,
+                  onPressed: () {
+                    StorageService.instance.storage.write("count", count >= 999999 ? count = 0 : count++);
+                    setState(() {});
+                  },
+                )),
+          ),
+          Positioned(
+            left: 165.w,
+            right: 10.w,
+            top: 390.w,
+            child: Container(
+                width: 50.w,
+                height: 50.h,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100.r)),
+                child: FloatingActionButton(
+                  backgroundColor: Colors.transparent,
+                  onPressed: () {
+                    StorageService.instance.storage.write("count", count = 0);
+                    setState(() {});
+                  },
+                )),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:tasbex/test_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+import 'data_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TestScreen(),
-    );
+    return
+      ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context , child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: child,
+          );
+        },
+        child:  DataScreen(),
+      );
   }
 }
+
+
+
+
+
